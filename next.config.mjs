c3dd8af6c08308bfcdb9f01900ab2f5d41f752f7
Nikita Sukhachev <traed.traed@mail.ru>
2024-07-03 11:46:43 +0700

    feat: add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/cosmiccowboys',
+                destination: 'https://nomis.cc/multichain',
+                permanent: true,
+            },
+            {
+                source: '/pinatacloud',
+                destination: 'https://nomis.cc/multichain',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
